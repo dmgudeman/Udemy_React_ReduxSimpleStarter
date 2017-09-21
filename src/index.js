d757f0af9d1e8a41b8467a0f2682689fb4d9e1f2
davidmgudeman <davidmgudeman@gmail.com>
2017-09-21 15:25:17 -0700

    added a search bar component and obtained a youtube appId

--- src/index.js
+++ src/index.js
@@ -1,9 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import SearchBar from './components/search_bar'
+
+const API_KEY = 'AIzaSyB19JCXCXqGs8xsMY0jeslR5ldQY-fvi08'
 
 // Make a component
 const App = () => {
-  return <div>Hi!</div>
+  return (
+    <div>
+      <SearchBar />
+    </div>
+  )
 }
 
 // Generate HTML, put it in the DOM so it shows
