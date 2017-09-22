88be1176c4ee318e91dabb2271314dc0f5fdea50
davidmgudeman <davidmgudeman@gmail.com>
2017-09-22 11:26:04 -0700

    got the youtube search to work.

--- src/index.js
+++ src/index.js
@@ -1,16 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import SearchBar from './components/search_bar'
+import YTSearch from 'youtube-api-search';
 
-const API_KEY = 'AIzaSyB19JCXCXqGs8xsMY0jeslR5ldQY-fvi08'
+import SearchBar from './components/search_bar';
+
+// const API_KEY = 'AIzaSyB19JCXCXqGs8xsMY0jeslR5ldQY-fvi08';
+const API_KEY = 'AIzaSyDugtxUyFSHAvHMoY5OPsrX7MIwcEQZeMA';
+
+
 
 // Make a component
 const App = () => {
+
+  YTSearch({key: API_KEY, term: 'surfboards'}, data => {
+    console.log("IIIIIIIIII   MMMMMM ");
+    console.log(data);
+  });
+
   return (
     <div>
       <SearchBar />
+      Hi there
     </div>
-  )
+  );
 }
 
 // Generate HTML, put it in the DOM so it shows
