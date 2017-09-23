cbe92a2b8b857ed6572762b695f759f7eb5c60b9
davidmgudeman <davidmgudeman@gmail.com>
2017-09-23 10:15:39 -0700

    refactored index.js to be a class

--- src/index.js
+++ src/index.js
@@ -1,29 +1,30 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
-
 import SearchBar from './components/search_bar';
 
-// const API_KEY = 'AIzaSyB19JCXCXqGs8xsMY0jeslR5ldQY-fvi08';
 const API_KEY = 'AIzaSyDugtxUyFSHAvHMoY5OPsrX7MIwcEQZeMA';
 
+class App extends Component {
+  constructor(props) {
+    super(props);
 
+    this.state = { videos: [] };
 
-// Make a component
-const App = () => {
+    YTSearch({key: API_KEY, term: 'surfboards'}, videos => {
+      this.setState({ videos });
+      console.log(videos);
+    });
+  }
 
-  YTSearch({key: API_KEY, term: 'surfboards'}, data => {
-    console.log("IIIIIIIIII   MMMMMM ");
-    console.log(data);
-  });
-
-  return (
-    <div>
-      <SearchBar />
-      Hi there
-    </div>
-  );
+  render() {
+    return (
+      <div>
+        <SearchBar />
+        Hi there
+      </div>
+    );
+  }
 }
 
-// Generate HTML, put it in the DOM so it shows
 ReactDOM.render(<App />, document.querySelector('.container'));
