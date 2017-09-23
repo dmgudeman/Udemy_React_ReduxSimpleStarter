ac56cc7626fba87bff34b920cd784a03de0d5415
davidmgudeman <davidmgudeman@gmail.com>
2017-09-23 16:00:21 -0700

    debounced the search function

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import YTSearch from 'youtube-api-search';
+import _ from 'lodash';
+import YTSearch from 'youtube-api-search'
 import SearchBar from './components/search_bar';
 import VideoList from './components/video_list';
 import VideoDetail from './components/video_detail';
@@ -15,8 +16,11 @@ class App extends Component {
       videos: [],
       selectedVideo: null
     };
+    this.videoSearch('surfboards');
+  }
 
-    YTSearch({key: API_KEY, term: 'surfboards'}, videos => {
+  videoSearch(term){
+    YTSearch({key: API_KEY, term: term}, videos => {
       this.setState({ 
         videos: videos,
         selectedVideo: videos[0]
@@ -25,9 +29,10 @@ class App extends Component {
   }
 
   render() {
+    const videoSearch = _.debounce((term) => { this.videoSearch(term) }, 300);
     return (
       <div>
-        <SearchBar />
+        <SearchBar onSearchTermChange={videoSearch}/>
         <VideoDetail video={this.state.selectedVideo} />
         <VideoList 
           onVideoSelect= {selectedVideo => this.setState({selectedVideo}) }
